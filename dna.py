486e1b5c9f5a8d300f632270824a54e4e88e1741
Anton Shekhov <sh.anton2111@gmail.com>
2015-01-07 16:27:58 +0100

    Changed the way Errors are raised for test

--- dna.py
+++ dna.py
@@ -1,21 +1,26 @@
 """ DNA module. Library to work with DNA sequence """
+import re
 
 class DNA_Error (Exception): pass
 class InvalidCharacterError (DNA_Error): pass
 class InvalidSequenceError (DNA_Error): pass
 
 NUCLEOTIDES = "ATGCU"
+N_PATTERN = '[^ATUGC]'
+
+def isNucleotide (sequence):
+	if not sequence.isupper(): raise InvalidCharacterError
+	if 'T' and 'U' in sequence: raise InvalidSequenceError
+	if re.search (N_PATTERN, sequence):	raise InvalidSequenceError
+
+
 
 def count_nucleotides (sequence):
 	""" Returns an array with quantity of nucleotides inside in the following order (A T G C) """
 	result = [0, 0, 0, 0] # A T G C
-	
-	if 'T' and 'U' in sequence: raise InvalidSequenceError	
-	if not sequence.isupper(): raise InvalidCharacterError
+	isNucleotide(sequence)
 	
 	for n in sequence:
-		if n.upper() not in NUCLEOTIDES: raise InvalidSequenceError
-		#if n.lower() == n: raise InvalidCharacterError
 		if n == 'A': result[0]+=1
 		elif n == 'T' or n == 'U': result[1]+=1
 		elif n == 'G': result[2]+=1
@@ -25,5 +30,5 @@ def count_nucleotides (sequence):
 	
 def dnaToRna (dna):
 	""" Replace Thymines to Uracils """
-	if 'T' and 'U' in dna: raise InvalidSequenceError		
+	isNucleotide(dna)	
 	return dna.replace('T', 'U')
